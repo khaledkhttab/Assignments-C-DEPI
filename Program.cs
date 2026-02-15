ad7d9ed3797ae9bdfc314d1c560defe6e2144ece
Khaled Khttab <KHALEDKHTTAB1@GMAIL.COM>
2026-02-15 12:35:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿namespace Assignment_01__C__
+{
+    class Person
+    {
+    public string Name;
+    }
+
+internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Print number
+            //Console.WriteLine("Enter number :");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(number);
+            //Console.ReadLine();
+            #endregion
+
+            #region Convert string to int
+            //string input = "12345678";
+            //int number = Convert.ToInt32(input);
+            //Console.WriteLine(number);
+            //Console.ReadLine(); 
+            #endregion
+
+            #region Add two float
+            //Console.WriteLine("enter first number");
+            //float num1 = Convert.ToSingle(Console.ReadLine());
+            //Console.WriteLine("enter second number");
+            //float num2 = Convert.ToSingle(Console.ReadLine());
+            //float sum = num1 + num2;
+            //Console.WriteLine($"The sum of {num1} and {num2} is {sum}");
+            //Console.ReadLine();
+            #endregion
+
+            #region Substring
+            //Console.WriteLine("Enter your password");
+            //string password = Console.ReadLine();
+            //Console.WriteLine($"First three char of your password is {password.Substring(0,3)}");
+            //Console.ReadLine(); 
+            #endregion
+
+            #region Assigning one value to another
+            //int x = 10;
+            //int y = x;
+
+            //y = 20;
+
+            //Console.WriteLine("x = " + x);
+            //Console.WriteLine("y = " + y);
+            //Console.ReadLine(); 
+            #endregion
+
+            #region Assigning one reference type to another
+            //Person P1 = new Person();
+            //P1.Name = "Khttab";
+            //Person P2 = P1;
+            //P2.Name = "Khaled";
+            //Console.WriteLine("P1 Name = " + P1.Name);
+            //Console.WriteLine("P2 Name = " + P2.Name);
+            //Console.ReadLine(); 
+            #endregion
+
+            #region Merge two string
+            //Console.WriteLine("Enter your first name:");
+            //string firstName = Console.ReadLine();
+            //Console.WriteLine("Enter your last name:");
+            //string lastName = Console.ReadLine();
+            //string fullName = firstName + " " + lastName;
+            //Console.WriteLine("Your full name is: " + fullName);
+            //Console.ReadLine(); 
+            #endregion
+            // 7 
+            // 8 (B) A value 1 will be assigned to d.
+            // 9 (D) 6 1
+            // 10 (D) 7 7
+
+        }
+    }
+}
